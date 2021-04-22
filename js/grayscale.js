1698a653f5787ad2fe719b2e55060e13f9e8d959
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2021-04-21 19:27:36 -0600

    Update to bootstrap 4
    Squashed commit of the following:
    
    commit 68a3a6636215ac9c9e2e47c766b4e85539a2bcf0
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 21 19:23:37 2021 -0600
    
        Fix toc hashing and links
    
    commit 0500ba51a8a0a6b0a5eaef772d2c196f5ae08323
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 21 19:22:52 2021 -0600
    
        Fix navbar on posts and docs
    
    commit 54f74fb708418c1a03b1aa656719d8b3e4e27dde
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 21 18:49:02 2021 -0600
    
        Fix column offsets
    
    commit 7b1cf4aa7d3f5884aac156c233edcff5c26d6a19
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 21 18:43:27 2021 -0600
    
        Fix navbar collapse and spacing
    
    commit adbb0f9c02979a1bddaf50d94c6fca9fa2c6f516
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 21 18:04:08 2021 -0600
    
        Fix navbar theming on main
    
    commit 171202873e6c107136647177295ce5468c55fb75
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 21 18:03:37 2021 -0600
    
        fixup! Update bootstrap
    
    commit 2a4452be1281f062bd116058243b889b769a8dc2
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Mon Apr 19 14:01:38 2021 -0600
    
        Nest docs section in col
    
    commit 3ad7fa65bea2363cd03a4be183a62934d5f0d9fc
    Author: Jamison Lofthouse <jamison.lofthouse@gmail.com>
    Date:   Wed Apr 7 11:20:22 2021 -0600
    
        Update bootstrap

--- js/grayscale.js
+++ js/grayscale.js
@@ -7,9 +7,9 @@
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar.fixed-top").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar.fixed-top").removeClass("top-nav-collapse");
     }
 });
 
@@ -31,7 +31,8 @@ $(function() {
         history: false,
         // scrollTo: 55,
         // highlightOffset: 5,
-        highlightDefault: false
+        highlightDefault: false,
+        hashGenerator: "pretty"
     });
     console.log(toc.options);
 });
